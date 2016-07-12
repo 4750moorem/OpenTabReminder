c38a7f7b6b745913d08f7ac01b5ba42ec32bcee5
4750moorem <4750moorem@gmail.com>
2016-07-12 14:01:00 -0500

    Mikes testbranch (#1)
    * I dont know what im doing
    
    * im adding meaningful comments

--- OpenTabReminder/OpenTabReminder/Views/ControlsRepository.cs
+++ OpenTabReminder/OpenTabReminder/Views/ControlsRepository.cs
@@ -19,6 +19,8 @@ namespace OpenTabReminder.Views
        }
        public Button GenerateOpenTabButton()
        {
+            //This is a button i made weee
+            //what happens now
            return new Button
            {
                Text = "Open Tab",
